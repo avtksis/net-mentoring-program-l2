2456b3b009c32c38b78261d31f23c64d640c21ce
Ihar Kuchynski <ihar.kuchynski@outlook.com>
2018-09-27 16:38:55 +0300

    Created structure of folders for module 1

---
+++ Module 1 - MultiThreading/Version 1 AsyncAwait/Async_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Async_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
