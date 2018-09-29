11a66315a8dfc659236b68dabdbfc6d331c136f0
Ihar Kuchynski <ihar.kuchynski@outlook.com>
2018-09-29 17:38:39 +0300

    completed task 2

--- Module 1 - MultiThreading/Version 1 Multithreading/Multithreading/MultithreadingTasks.cs
+++ Module 1 - MultiThreading/Version 1 Multithreading/Multithreading/MultithreadingTasks.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyModel;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -32,7 +35,7 @@ namespace Multithreading
         }
 
         [Fact]
-        public async void Task2()
+        public void Task2()
         {
             void ArrayPrint(int[] array, string description) => _output.WriteLine($"{description}: {string.Join(", ", array)}");
 
@@ -46,13 +49,13 @@ namespace Multithreading
 
                 return sequence;
             });
-
+            
             int[] MultiplyArray(int[] sequence)
             {
-                var random = new Random();
+                var randomMultiplier = new Random().Next(10);
 
-                sequence = sequence.Select(item => item * random.Next(10)).ToArray();
-                ArrayPrint(sequence, "Multiplied array:");
+                sequence = sequence.Select(item => item * randomMultiplier).ToArray();
+                ArrayPrint(sequence, $"Multiplied array ({randomMultiplier})");
 
                 return sequence;
             };
@@ -60,44 +63,88 @@ namespace Multithreading
             int[] SortArray(int[] sequence)
             {
                 sequence = sequence.OrderBy(item => item).ToArray();
-                ArrayPrint(sequence, "Sorted array:");
+                ArrayPrint(sequence, "Sorted array");
 
                 return sequence;
             };
 
             void PrintAverage(int[] sequence) => _output.WriteLine($"Average of array: {sequence.Average()}");
 
-           createArrayTask
+            var tasksChain = createArrayTask
                 .ContinueWith(task => MultiplyArray(task.Result))
                 .ContinueWith(task => SortArray(task.Result))
-                ;//.ContinueWith(task => PrintAverage(task.Result));
-            
-            //createArrayTask.Start();
-            //var createArrayTask = new Task(() =>
-            //{
-            //    for (int i = 0; i < 10; i++) sequence[i] = random.Next(0, 10);
-            //    ArrayPrint(sequence, "Randomly generated array");
-            //});
+                .ContinueWith(task => PrintAverage(task.Result));
 
-            //var multiplyArrayTask = new Task(() =>
-            //{
-            //    sequence = sequence.Select(item => item * random.Next(10)).ToArray();
-            //    ArrayPrint(sequence, "Multiplied array:");
-            //});
+            createArrayTask.Start();
+            tasksChain.Wait();
+        }
 
-            //var sortArrayTask = new Task(() =>
-            //{
-            //    sequence = sequence.OrderBy(item => item).ToArray();
-            //    ArrayPrint(sequence, "Sorted array:");
-            //});
+        [Fact]
+        public void Task3()
+        {
+            int[,] CreateMatrice(int mSize, int nSize)
+            {
+                var matrice = new int[mSize, nSize];
+                var random = new Random();
 
-            //var printAverageTask = new Task(() => _output.WriteLine($"Average of array: {sequence.Average()}") );
+                for (int m = 0; m < mSize; m++)
+                    for (int n = 0; n < nSize; n++)
+                        matrice[m, n] = random.Next(10);
 
-            //var res = CreateArrayTask.ContinueWith(task1 => 
-            //        multiplyArrayTask.ContinueWith(task2 => 
-            //            sortArrayTask.ContinueWith(task3 => printAverageTask))).Result;
+                return matrice;
+            }
 
+            var matriceFirst = CreateMatrice(5, 5);
+            var matriceSecond = CreateMatrice(5, 5);
 
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void Task4()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void Task5()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void Task6()
+        {
+            var sequence = new List<int>();
+
+            List<int> SafeAccess()
+            {
+                object locker = new object();
+
+                lock (locker)
+                {
+                    return sequence;
+                }
+            }
+
+         
+
+            new Thread((object obj) =>
+            {
+                int elementCount = sequence.Count;
+
+                while (elementCount <= 10 && elementCount > 0)
+                {
+                    _output.WriteLine(SafeAccess().Last().ToString());
+                }
+            }).Start();
+
+            new Thread((object obj) =>
+            {
+                for (int i = 0; i < 10; i++) SafeAccess().Add(i);
+            }).Start();
+
+            Thread.Sleep(5000);
         }
     }
 }
