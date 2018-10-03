2170244b67f7fc824b4e63461e2c80016bd42577
Ihar Kuchynski <ihar.kuchynski@outlook.com>
2018-10-03 18:41:14 +0300

    beta implementation of client and server for V2 task

--- Module 1 - MultiThreading/Version 2/Task 1/Client/Program.cs
+++ Module 1 - MultiThreading/Version 2/Task 1/Client/Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 
 namespace Client
 {
     class Program
     {
+        private static readonly IPHostEntry IpHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+        private static readonly IPAddress IpAddress = IpHostInfo.AddressList[0];
+        private static readonly IPEndPoint EndPoint = new IPEndPoint(IpAddress, 11000);
+
+        private static readonly Socket SocketSender = new Socket(IpAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                SocketSender.Connect(EndPoint);
+
+                Console.WriteLine("Socket connected to {0}", SocketSender.RemoteEndPoint);
+
+                // Encode the data string into a byte array.  
+                byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
+
+                // Send the data through the socket.  
+                for (int i = 0; i < 10; i++)
+                {
+                    int bytesSent = SocketSender.Send(msg);
+                    Thread.Sleep(1000);
+                }
+                
+                byte[] bytes = new byte[1024];
+
+                // Receive the response from the remote device.  
+                int bytesRec = SocketSender.Receive(bytes);
+                Console.WriteLine("Echoed test = {0}", Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                // Release the socket.  
+                SocketSender.Shutdown(SocketShutdown.Both);
+                SocketSender.Close();
+
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+
+            Console.Read();
         }
     }
 }
--- Module 1 - MultiThreading/Version 2/Task 1/Server/Program.cs
+++ Module 1 - MultiThreading/Version 2/Task 1/Server/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Server
 {
     class Program
     {
+        private const int HistoryMessagesCount = 3;
+        private static readonly IPHostEntry IpHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+        private static readonly IPAddress IpAddress = IpHostInfo.AddressList[0];
+        private static readonly IPEndPoint LocalEndPoint = new IPEndPoint(IpAddress, 11000);
+
+        private static readonly Socket SocketListener = new Socket(IpAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                SocketListener.Bind(LocalEndPoint);
+                SocketListener.Listen(10);
+
+                // Start listening for connections.  
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection...");
+                    // Program is suspended while waiting for an incoming connection.  
+                    Socket handler = SocketListener.Accept();
+
+                    var data = String.Empty;
+                    var bytes = new Byte[1024];
+
+                    // An incoming connection needs to be processed.  
+                    while (true)
+                    {
+                        int bytesRec = handler.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                        if (data.IndexOf("<EOF>", StringComparison.Ordinal) > -1) break;
+
+                    }
+
+                    // Show the data on the console.  
+                    Console.WriteLine("Text received : {0}", data);
+
+                    // Echo the data back to the client.  
+                    byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    handler.Send(msg);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
